0208e8df444725a58904503b686461cfb6f1685c
fairuztahir <mohdfairuztahir@gmail.com>
2022-02-28 10:49:23 +0800

    feat: lint and prettier

---
+++ web/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true
+    },
+    extends: [
+        'plugin:vue/essential',
+        'eslint:recommended',
+        'plugin:prettier/recommended'
+    ],
+    parserOptions: {
+        ecmaVersion: 2020
+    },
+    plugins: ['prettier'],
+    rules: {
+        'prettier/prettier': 'error',
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    }
+}
