297aa1be87ef0c2d869146d43c9b5e63a2edafc8
fairuztahir <mohdfairuztahir@gmail.com>
2022-03-01 20:13:19 +0800

    refactor: web because of the sass not compatible with vuetify

--- web/src/pages/Dashboard/DashboardMain.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div>
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam,
-    similique, quo recusandae placeat dicta asperiores modi sint ea.
-  </div>
-</template>
