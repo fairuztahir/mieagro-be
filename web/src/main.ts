be160d5fd002e3ea4d52b46227e84ab59aebd392
fairuztahir <mohdfairuztahir@gmail.com>
2022-03-04 12:11:48 +0800

    feat: material design

--- web/src/main.ts
+++ web/src/main.ts
@@ -2,10 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import vuetify from '@/plugins/vuetify'
 import routes from '@/routes'
-// import { loadFonts } from './plugins/webfontloader'
 
 const app = createApp(App)
-// loadFonts()
 
 // Assign Global
 // app.config.globalProperties.pageTitle = 'Home1'
--- web/src/plugins/vuetify.ts
+++ web/src/plugins/vuetify.ts
@@ -15,29 +15,31 @@ const theme = {
   info: '#00CAE3'
 }
 
+// colors: {
+//   background: '#9E9E9E',
+//   surface: '#FFFFFF',
+//   primary: '#6200EE',
+//   'primary-darken-1': '#3700B3',
+//   secondary: '#03DAC6',
+//   'secondary-darken-1': '#018786',
+//   error: '#B00020',
+//   info: '#2196F3',
+//   success: '#4CAF50',
+//   warning: '#FB8C00'
+// }
+
 const myCustomLightTheme: ThemeDefinition = {
-  dark: false,
-  colors: {
-    background: '#9E9E9E',
-    surface: '#FFFFFF',
-    primary: '#6200EE',
-    'primary-darken-1': '#3700B3',
-    secondary: '#03DAC6',
-    'secondary-darken-1': '#018786',
-    error: '#B00020',
-    info: '#2196F3',
-    success: '#4CAF50',
-    warning: '#FB8C00'
-  }
+  // dark: false,
+  colors: theme
 }
 
 const vuetify = createVuetify({
   components,
   directives,
   theme: {
-    defaultTheme: 'myCustomLightTheme',
     themes: {
-      myCustomLightTheme
+      dark: myCustomLightTheme,
+      light: myCustomLightTheme
     }
   }
 })
