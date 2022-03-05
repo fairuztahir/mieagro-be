865461e70cb065fc824fc76d717ca7e87b485882
fairuztahir <mohdfairuztahir@gmail.com>
2022-03-05 16:23:06 +0800

    refactor: vuetify footer scss

--- web/src/views/Dashboard/DashboardMain.vue
+++ web/src/views/Dashboard/DashboardMain.vue
@@ -1,8 +1,10 @@
 <template>
-  <div class="mx-auto">
+  <!-- <div class="mx-auto">
     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam,
     similique, quo recusandae placeat dicta asperiores modi sint ea.
-  </div>
+  </div> -->
+  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam,
+    similique, quo recusandae placeat dicta asperiores modi sint ea.
 </template>
 
 <script lang="ts">
