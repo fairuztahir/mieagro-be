0590c28b7459f3e13e20abf1ba9d801c78ccbb65
fairuztahir <mohdfairuztahir@gmail.com>
2022-03-05 15:36:23 +0800

    refactor: footer position and width

--- web/src/views/Dashboard/Layout/FooterLayout.vue
+++ web/src/views/Dashboard/Layout/FooterLayout.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-footer app bottom right fixed class="d-flex flex-column">
+  <v-footer app bottom fixed class="d-flex flex-column test">
     <div class="text-black text-body-1 font-weight-light pt-6 pt-md-0 text-right">
       &copy; {{ new Date().getFullYear() }} — <a to="/">Mie Agro</a>
     </div>
@@ -10,3 +10,4 @@
 import { defineComponent } from 'vue'
 export default defineComponent({})
 </script>
+
